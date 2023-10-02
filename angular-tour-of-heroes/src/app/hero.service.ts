7cb109c1df945d2420f2645b1f3483eae04533f8
Dinosault <cody911110@gmail.com>
2023-10-03 01:12:23 +0800

    dachboard

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -9,12 +9,20 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  
+  // 將ＭessageService 注入
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    this.messageService.add('HeroService: fetched hero');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id ===id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
